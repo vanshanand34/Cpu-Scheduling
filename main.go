ec16ce2d466312e424a11564b3f40918b82a9dc3
Vansh Anand <anandvansh34@gmail.com>
2025-07-22 16:28:54 +0530

    chore: Added priority data to processes and minor changes

--- main.go
+++ main.go
@@ -1,29 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type Process struct {
-	arrival float64
-	burst   float64
+	arrival  float64
+	burst    float64
+	priority int
 }
 
 func main() {
 
 	var processes []Process = []Process{
-		{0, 1}, {1, 7}, {2, 22}, {3, 3}, {4, 9}, {5, 15}, {6, 26}, {7, 17}, {8, 8}, {9, 19},
+		{0, 1, 2}, {1, 7, 3}, {2, 22, 12}, {3, 3, 5}, {4, 9, 11}, {5, 15, 9}, {6, 26, 7}, {7, 17, 6}, {8, 8, 1}, {9, 19, 8},
 	}
 
 	firstComeFirstServe(processes)
 	shortestJobFirst(processes)
 	longestJobFirst(processes)
 	highestResponseRatioNext(processes)
+	priorityScheduling(processes)
+	nonPreemptivePriorityScheduling(processes)
 
 }
 
 func printAlgoResult(algo_name string, avg_wait_time, avg_tat_time float64) {
 	fmt.Println()
-	fmt.Println("--- ", algo_name, " ---")
+	fmt.Println(strings.Repeat("-", 40))
+	fmt.Println(algo_name)
 	fmt.Println("Average waiting time: ", avg_wait_time)
 	fmt.Println("Average turn around time: ", avg_tat_time)
+	fmt.Println(strings.Repeat("-", 40))
 	fmt.Println()
 }
