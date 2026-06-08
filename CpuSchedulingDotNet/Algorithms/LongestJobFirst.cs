cb73df3338cc1271a73924aea2e2d3b29d301a96
Vansh Anand <anandvansh34@gmail.com>
2026-06-09 00:42:48 +0530

    fix: Fix the logic of Longest job first in dotnet implementation

--- CpuSchedulingDotNet/Algorithms/LongestJobFirst.cs
+++ CpuSchedulingDotNet/Algorithms/LongestJobFirst.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace CpuSchedulingDotNet.Algorithms
 {
@@ -6,21 +7,50 @@ namespace CpuSchedulingDotNet.Algorithms
   {
     public static SchedulingResult Execute(Process[] processes)
     {
-      var sortedProcesses = processes.OrderBy(p => p.BurstTime).Reverse().ToArray();
-      Console.WriteLine(sortedProcesses.ToString());
+      var sortedProcesses = processes.OrderBy(p => p.ArrivalTime).ToArray();
+      bool[] completed = new bool[sortedProcesses.Length];
+
       double totalTurnaroundTime = 0;
       double totalWaitTime = 0;
       double currTime = 0;
 
-      foreach (var process in sortedProcesses)
-      {
-        double completionTime = Math.Max(currTime, process.ArrivalTime) + process.BurstTime;
-        double turnaroundTime = completionTime - process.ArrivalTime;
-        double waitTime = turnaroundTime - process.BurstTime;
+      int completedCount = 0;
+      int largestBurstIdx = 0;
 
-        totalWaitTime += waitTime;
+      while (completedCount < processes.Length)
+      {
+        largestBurstIdx = -1;
+        for (int i = 0; i < sortedProcesses.Length; i++)
+        {
+          var process = sortedProcesses[i];
+
+          if (completed[i])
+            continue;
+
+          if (
+            process.ArrivalTime <= currTime &&
+            (largestBurstIdx == -1 || process.BurstTime > sortedProcesses[largestBurstIdx].BurstTime)
+          )
+            largestBurstIdx = i;
+
+        }
+
+        if (largestBurstIdx == -1)
+        {
+          currTime++;
+          continue;
+        }
+
+        var currProcess = sortedProcesses[largestBurstIdx];
+        double completionTime = currTime + currProcess.BurstTime;
+        double turnaroundTime = completionTime - currProcess.ArrivalTime;
+        double waitTime = turnaroundTime - currProcess.BurstTime;
+
         totalTurnaroundTime += turnaroundTime;
+        totalWaitTime += waitTime;
         currTime = completionTime;
+        completed[largestBurstIdx] = true;
+        completedCount++;
       }
 
       return new SchedulingResult(totalWaitTime / sortedProcesses.Length, totalTurnaroundTime / sortedProcesses.Length);
